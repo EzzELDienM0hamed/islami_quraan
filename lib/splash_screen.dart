5f52390870564b5c5d83496e5c69b4ee6296fe3a
EzzELDienM0hamed <161841074+EzzELDienM0hamed@users.noreply.github.com>
2024-03-02 12:12:01 +0200

    Update splash_screen.dart

--- lib/splash_screen.dart
+++ lib/splash_screen.dart
@@ -6,7 +6,6 @@ class SplashScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
       body: const Text('splash screen'),
     );
   }
