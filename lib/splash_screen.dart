1cad432158c65957bc80769acfb720a95f190325
Ezz EL-Deen Mohamed <149920923+ezz-e4n@users.noreply.github.com>
2024-03-02 15:20:12 +0200

    Update splash_screen.dart

--- lib/splash_screen.dart
+++ lib/splash_screen.dart
@@ -6,7 +6,6 @@ class SplashScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
       body: Text('0000'),
       backgroundColor: Colors.red,
       floatingActionButton: FloatingActionButton(onPressed: () {}),
